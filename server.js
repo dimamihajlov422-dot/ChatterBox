8222a91118fd030a146bef790bb593fc88770fbf
dimamihajlov422-dot <dimamihajlov422@gmail.com>
2026-03-13 15:19:51 +0300

    fix server

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 let messages = [];
 
-io.on('connection', (socket) => {
-    console.log('Пользователь подключился');
+io.on("connection", (socket) => {
+    console.log("Пользователь подключился");
 
-    // отправляем старые сообщения новому пользователю
     messages.forEach((msg) => {
-        socket.emit('chat message', msg);
+        socket.emit("chat message", msg);
     });
 
-    socket.on('chat message', (data) => {
-        messages.push(data); // сохраняем сообщение
-        io.emit('chat message', data); // отправляем всем
+    socket.on("chat message", (data) => {
+        messages.push(data);
+        io.emit("chat message", data);
     });
 
-    socket.on('disconnect', () => {
-        console.log('Пользователь отключился');
+    socket.on("disconnect", () => {
+        console.log("Пользователь отключился");
     });
 });
 
 http.listen(PORT, () => {
-    console.log(Сервер запущен на порту ${PORT});
+    console.log("Сервер запущен на порту " + PORT);
 });
