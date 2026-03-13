3306e4131c679744fe2dcd0a2d53e7af63389496
dimamihajlov422-dot <dimamihajlov422@gmail.com>
2026-03-13 17:28:18 +0300

    contact 2

--- server.js
+++ server.js
@@ -2,43 +2,50 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
-// Онлайн-пользователи: { socket.id: nick }
-const onlineUsers = {};
-
-// Приватные комнаты: { roomId: [{nick,msg}, ...] }
-const privateRooms = {};
-
-// Общий чат (только читать)
+// Мини-хранилище пользователей и паролей
+const users = {}; // { nick: password }
+const onlineUsers = {}; // { socket.id: nick }
+const privateRooms = {}; // { roomId: [{nick,msg}, ...] }
 const generalChat = [];
 
+// API регистрации
+app.post("/register", (req, res) => {
+    const { nick, password } = req.body;
+    if (!nick || !password) return res.status(400).send({ error: "Заполните поля" });
+    if (users[nick]) return res.status(400).send({ error: "Ник занят" });
+    users[nick] = password;
+    return res.send({ success: true });
+});
+
+// API входа
+app.post("/login", (req, res) => {
+    const { nick, password } = req.body;
+    if (!nick || !password) return res.status(400).send({ error: "Заполните поля" });
+    if (users[nick] !== password) return res.status(400).send({ error: "Неверный ник или пароль" });
+    return res.send({ success: true });
+});
+
 io.on("connection", socket => {
-    // Получаем ник
     socket.on("set nick", nick => {
         onlineUsers[socket.id] = nick;
-
-        // Обновляем список контактов у всех
         io.emit("update users", Object.values(onlineUsers));
-
-        // Отправляем историю общего чата
         socket.emit("general chat", generalChat);
     });
 
-    // Приватные сообщения
     socket.on("private message", data => {
-        // data: { toNick, msg, fromNick }
         const roomId = [data.fromNick, data.toNick].sort().join("_");
         if (!privateRooms[roomId]) privateRooms[roomId] = [];
         privateRooms[roomId].push({ nick: data.fromNick, msg: data.msg });
 
-        // Найти socket.id получателя
         const toSocketId = Object.keys(onlineUsers).find(id => onlineUsers[id] === data.toNick);
 
-        // Отправить обоим
         socket.emit("private message", { fromNick: data.fromNick, msg: data.msg, toNick: data.toNick });
         if (toSocketId) io.to(toSocketId).emit("private message", { fromNick: data.fromNick, msg: data.msg, toNick: data.toNick });
     });
