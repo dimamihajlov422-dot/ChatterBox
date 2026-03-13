5e2f3c5e69e8b5e661cb1d0b9f40fd0847ce9f26
dimamihajlov422-dot <dimamihajlov422@gmail.com>
2026-03-13 15:16:33 +0300

    Добавил память сообщений

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js — полностью рабочий вариант для ChatterBox
-
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -7,28 +5,28 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
-// Отдаём все файлы из папки public
 app.use(express.static('public'));
 
-// Когда подключается новый пользователь
-et messages = [ ] ;
+let messages = [];
+
 io.on('connection', (socket) => {
-    console.log('Новый пользователь подключился');
-messages.forEach((msg) => {
-    socket.emit('chat message', msg);
-});
-    // Получаем объект с ником и сообщением
+    console.log('Пользователь подключился');
+
+    // отправляем старые сообщения новому пользователю
+    messages.forEach((msg) => {
+        socket.emit('chat message', msg);
+    });
+
     socket.on('chat message', (data) => {
-    messages.push(data); // сохраняем сообщение
-    io.emit('chat message', data); // отправляем всем
-});
+        messages.push(data); // сохраняем сообщение
+        io.emit('chat message', data); // отправляем всем
+    });
 
     socket.on('disconnect', () => {
         console.log('Пользователь отключился');
     });
 });
 
-// Запуск сервера
 http.listen(PORT, () => {
     console.log(Сервер запущен на порту ${PORT});
 });
